dd9fa51e47270447059c6515445fd50b101ead44
Nakata <ryosuke.nakata@acompany-ac.com>
2022-09-09 14:40:53 +0900

    Add request log

--- quickmpc/qmpc.py
+++ quickmpc/qmpc.py
@@ -1,3 +1,4 @@
+import logging
 from dataclasses import dataclass, field, InitVar
 from typing import Dict, List, Tuple
 
@@ -9,6 +10,7 @@ from .share import Share
 from .utils.parse_csv import (parse, parse_csv, parse_csv_to_bitvector,
                               parse_to_bitvector)
 
+logger = logging.getLogger(__name__)
 # qmpc.JobStatus でアクセスできるようにエイリアスを設定する
 JobStatus: enum_type_wrapper.EnumTypeWrapper \
     = common_types_pb2.JobStatus
@@ -29,6 +31,7 @@ class QMPC:
 
     def __post_init__(self, endpoints: List[str],
                       token: str):
+        logger.info(f"[QuickMPC server IP]={endpoints}")
         object.__setattr__(self, "_QMPC__qmpc_server", QMPCServer(
             endpoints, token))
         object.__setattr__(self, "_QMPC__party_size", len(endpoints))
@@ -54,69 +57,126 @@ class QMPC:
     def send_share(self, secrets: List, schema: List[str],
                    matching_column: int = 1,
                    piece_size: int = 1_000_000) -> Dict:
+        logger.info("send_share request. "
+                    f"[secrets size]={len(secrets)}x{len(secrets[0])} "
+                    f"[matching ID name]={schema[matching_column-1]}")
         return self.__qmpc_server.send_share(
             secrets, schema, matching_column, piece_size)
 
     def delete_share(self, data_ids: List[str]) -> Dict:
+        logger.info("delete_share request. "
+                    f"[delete id list]={data_ids}")
         return self.__qmpc_server.delete_share(data_ids)
 
     def mean(self, join_order: Tuple[List, List, List],
              src: List) -> Dict:
+        logger.info("mean request. "
+                    f"[data_id list]={join_order[0]} "
+                    f"[join method]={join_order[1]} "
+                    f"[matching ID columns]={join_order[2]} "
+                    f"[src columns]={src}")
         return self.__qmpc_server.execute_computation(
             ComputationMethod.Value("COMPUTATION_METHOD_MEAN"),
             join_order, (src, []))
 
     def variance(self, join_order: Tuple[List, List, List],
                  src: List) -> Dict:
+        logger.info("variance request. "
+                    f"[data_id list]={join_order[0]} "
+                    f"[join method]={join_order[1]} "
+                    f"[matching ID columns]={join_order[2]} "
+                    f"[src columns]={src}")
         return self.__qmpc_server.execute_computation(
             ComputationMethod.Value("COMPUTATION_METHOD_VARIANCE"),
             join_order, (src, []))
 
     def sum(self, join_order: Tuple[List, List, List], src: List) -> Dict:
+        logger.info("sum request. "
+                    f"[data_id list]={join_order[0]} "
+                    f"[join method]={join_order[1]} "
+                    f"[matching ID columns]={join_order[2]} "
+                    f"[src columns]={src}")
         return self.__qmpc_server.execute_computation(
             ComputationMethod.Value("COMPUTATION_METHOD_SUM"),
             join_order, (src, []))
 
     def correl(self, join_order: Tuple[List, List, List],
                inp: Tuple[List, List]) -> Dict:
+        logger.info("correl request. "
+                    f"[data_id list]={join_order[0]} "
+                    f"[join method]={join_order[1]} "
+                    f"[matching ID columns]={join_order[2]} "
+                    f"[src columns]={inp[0]}"
+                    f"[target columns]={inp[1]}")
         return self.__qmpc_server.execute_computation(
             ComputationMethod.Value("COMPUTATION_METHOD_CORREL"),
             join_order, inp)
 
     def linear_regression(self, join_order: Tuple[List, List, List],
                           inp: Tuple[List, List]) -> Dict:
+        logger.info("linear_regression request. "
+                    f"[data_id list]={join_order[0]} "
+                    f"[join method]={join_order[1]} "
+                    f"[matching ID columns]={join_order[2]} "
+                    f"[design variable columns]={inp[0]}"
+                    f"[objective variable columns]={inp[1]}")
         return self.__qmpc_server.execute_computation(
             ComputationMethod.Value("COMPUTATION_METHOD_LINEAR_REGRESSION"),
             join_order, inp)
 
     def logistic_regression(self, join_order: Tuple[List, List, List],
                             inp: Tuple[List, List]) -> Dict:
+        logger.info("logistic_regression request. "
+                    f"[data_id list]={join_order[0]} "
+                    f"[join method]={join_order[1]} "
+                    f"[matching ID columns]={join_order[2]} "
+                    f"[design variable columns]={inp[0]}"
+                    f"[objective variable columns]={inp[1]}")
         return self.__qmpc_server.execute_computation(
             ComputationMethod.Value("COMPUTATION_METHOD_LOGISTIC_REGRESSION"),
             join_order, inp)
 
     def meshcode(self, join_order: Tuple[List, List, List],
                  src: List) -> Dict:
+        logger.info("meshcode request. "
+                    f"[data_id list]={join_order[0]} "
+                    f"[join method]={join_order[1]} "
+                    f"[matching ID columns]={join_order[2]} "
+                    f"[src columns]={src}")
         return self.__qmpc_server.execute_computation(
             ComputationMethod.Value("COMPUTATION_METHOD_MESH_CODE"),
             join_order, (src, []))
 
     def decision_tree(self, join_order: Tuple[List, List, List],
                       inp: Tuple[List, List]) -> Dict:
+        logger.info("decision_tree request. "
+                    f"[data_id list]={join_order[0]} "
+                    f"[join method]={join_order[1]} "
+                    f"[matching ID columns]={join_order[2]} "
+                    f"[design variable columns]={inp[0]}"
+                    f"[objective variable columns]={inp[1]}")
         return self.__qmpc_server.execute_computation(
             ComputationMethod.Value("COMPUTATION_METHOD_DECISION_TREE"),
             join_order, inp)
 
     def get_join_table(self, join_order: Tuple[List, List, List]) -> Dict:
+        logger.info("get_join_table request. "
+                    f"[data_id list]={join_order[0]} "
+                    f"[join method]={join_order[1]} "
+                    f"[matching ID columns]={join_order[2]}")
         return self.__qmpc_server.execute_computation(
             ComputationMethod.Value("COMPUTATION_METHOD_JOIN_TABLE"),
             join_order, (join_order[2], []))
 
     def get_computation_result(self, job_id: str) -> Dict:
+        logger.info("get_computation_result request. "
+                    f"[job_id]={job_id}")
         return self.__qmpc_server.get_computation_result(job_id)
 
     def send_model_params(self, params: list,
                           piece_size: int = 1_000_000) -> Dict:
+        logger.info("send_model_params request. "
+                    f"[params size]={len(params)}")
         return self.__qmpc_server.send_model_params(
             params, piece_size)
 
@@ -124,6 +184,11 @@ class QMPC:
                                   model_param_job_uuid: str,
                                   join_order: Tuple[List, List, List],
                                   src: List[int]) -> Dict:
+        logger.info("linear_regression_predict request. "
+                    f"[data_id list]={join_order[0]} "
+                    f"[join method]={join_order[1]} "
+                    f"[matching ID columns]={join_order[2]} "
+                    f"[design variable columns]={src}")
         return self.__qmpc_server.predict(
             model_param_job_uuid,
             PredictMethod.Value("PREDICT_METHOD_LINEAR_REGRESSION"),
@@ -133,6 +198,11 @@ class QMPC:
                                     model_param_job_uuid: str,
                                     join_order: Tuple[List, List, List],
                                     src: List[int]) -> Dict:
+        logger.info("logistic_regression_predict request. "
+                    f"[data_id list]={join_order[0]} "
+                    f"[join method]={join_order[1]} "
+                    f"[matching ID columns]={join_order[2]} "
+                    f"[design variable columns]={src}")
         return self.__qmpc_server.predict(
             model_param_job_uuid,
             PredictMethod.Value("PREDICT_METHOD_LOGISTIC_REGRESSION"),
@@ -142,6 +212,11 @@ class QMPC:
                               model_param_job_uuid: str,
                               join_order: Tuple[List, List, List],
                               src: List[int]) -> Dict:
+        logger.info("decision_tree_predict request. "
+                    f"[data_id list]={join_order[0]} "
+                    f"[join method]={join_order[1]} "
+                    f"[matching ID columns]={join_order[2]} "
+                    f"[design variable columns]={src}")
         return self.__qmpc_server.predict(
             model_param_job_uuid,
             PredictMethod.Value("PREDICT_METHOD_DECISION_TREE"),
@@ -151,6 +226,11 @@ class QMPC:
                           model_param_job_id: str,
                           join_order: Tuple[List, List, List],
                           src: List[int]) -> Dict:
+        logger.info("sid3_tree_predict request. "
+                    f"[data_id list]={join_order[0]} "
+                    f"[join method]={join_order[1]} "
+                    f"[matching ID columns]={join_order[2]} "
+                    f"[design variable columns]={src}")
         return self.__qmpc_server.predict(
             model_param_job_id,
             PredictMethod.Value("PREDICT_METHOD_SID3_TREE"),
@@ -158,8 +238,11 @@ class QMPC:
 
     def get_data_list(self) \
             -> Dict:
+        logger.info("get_data_list request.")
         return self.__qmpc_server.get_data_list()
 
     def demo_sharize(self, secrets: List) -> Dict:
+        logger.info("demo_sharize request. "
+                    f"[secrets size]={len(secrets)}x{len(secrets[0])}")
         share = Share.sharize(secrets, self.__party_size)
         return {'is_ok': True, 'results': share}
