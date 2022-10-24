7c559608228a8f99a5d5fdecfdb23e6410d9aa1a
Shingo Ohtsuka <capriciouswalk@gmail.com>
2022-10-24 11:41:46 +0000

    WIP: add hjoin demo for debug

---
+++ demo/integration_demo/hjoin_demo.py
@@ -0,0 +1,90 @@
+""" QuickMPCによる演算の動作デモ
+本デモコードは次の操作を行っている．
+Step 1. QuickMPC動作のための準備
+Step 2. シェアをコンテナに送信
+Step 3. 計算リクエストを送信
+Step 4. シェアを元に復元された演算結果の真値を取得
+"""
+
+import logging
+import time
+from typing import Callable, List, Tuple
+
+from utils import make_statuses_detailed
+
+from google.protobuf import json_format
+import tabulate
+
+from quickmpc import QMPC
+
+logger = logging.getLogger(__name__)
+formatter = ("%(levelname)-5s | %(message)s")
+logging.basicConfig(level=logging.DEBUG, format=formatter)
+
+
+def large_data(size: int, data_num: int):
+    schema_size: int = 10
+    schema: List[str] = ["id"] + \
+        [f"s{i}_{data_num}" for i in range(schema_size - 1)]
+
+    mod = 47
+    data: List[List[str]] = [
+        [str(i)] + [str(((i * size + k) % mod) / mod)
+                    for k in range(schema_size - 1)]
+        for i in range(size)
+    ]
+    return qmpc.parse_csv_data([schema] + data)
+
+
+def get_data_id():
+    val: dict = {}
+
+    def get(size: int, data_num):
+        if (size, data_num) in val:
+            return val[(size, data_num)]
+        secrets, schema = large_data(size, data_num)
+        res = qmpc.send_share(secrets, schema)
+        assert (res["is_ok"])
+        data_id: str = res["data_id"]
+        val[(size, data_num)] = (data_id, secrets, schema)
+        return val[(size, data_num)]
+    return lambda size, data_num=1: get(size, data_num)
+
+
+if __name__ == '__main__':
+
+    """ Step 1. 諸準備 """
+    # QuickMPC Setting
+    qmpc: QMPC = QMPC(
+        ["http://localhost:9001",
+         "http://localhost:9002",
+         "http://localhost:9003"]
+    )
+    # データ Setting
+    size = 50000
+    data_id1, secrets1, schema1 = get_data_id()(size, data_num=1)
+    data_id2, secrets2, schema2 = get_data_id()(size, data_num=2)
+
+    table = ([data_id1, data_id2], [2], [1, 1])
+    exec_res = qmpc.get_join_table(table)
+
+    job_uuid = exec_res["job_uuid"]
+    logger.info(f"job_uuid: {job_uuid}")
+
+    while True:
+        # 計算が終わるか100秒(1秒x100回)経つまで繰り返す
+        get_res = qmpc.get_computation_result(job_uuid)
+
+        statuses = make_statuses_detailed(get_res['statuses'])
+        logger.info(f"statuses: {statuses}")
+        progresses = get_res['progresses']
+        print('---- >8 ----')
+        if progresses is not None:
+            progresses = [json_format.MessageToDict(p) for p in progresses]
+            print(tabulate.tabulate(progresses))
+        print('------------')
+
+        if get_res["results"] is not None:
+            break
+
+        time.sleep(1)
