596d120412ec36851c601490ffbdc776885dd6f6
Kota Takahashi <91713065+KotaTakahashi9320@users.noreply.github.com>
2022-12-16 14:51:54 +0900

    Fix if_present to support variable length arguments

--- quickmpc/utils/if_present.py
+++ quickmpc/utils/if_present.py
@@ -2,8 +2,9 @@ from typing import Any, Callable, Optional
 
 
 def if_present(optional: Optional[Any],
-               func: Callable[[Any], Any]
+               func: Callable[[Any], Any],
+               *arg: Any
                ) -> Optional[Any]:
     if optional is None:
         return None
-    return func(optional)
+    return func(optional, *arg)
